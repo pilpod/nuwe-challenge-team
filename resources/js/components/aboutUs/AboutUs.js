54bda5661595240d63917e265b297c6963e200ae
Moi <mmena4@hotmail.com>
2021-01-24 16:16:43 +0100

    responsive done

--- resources/js/components/aboutUs/AboutUs.js
+++ resources/js/components/aboutUs/AboutUs.js
@@ -6,11 +6,11 @@ function AboutUs() {
     return (
         <div className="aboutUs-container">
                 <h2>Sobre nosotros</h2>
-                <p>PERSEO, cuenta con más de 20 años de experiencia en el sector de Recursos humanos, conectado a los mejores candidatos con empresas punteras del sector TIC.
+                <p class="info">PERSEO, cuenta con más de 20 años de experiencia en el sector de Recursos humanos, conectado a los mejores candidatos con empresas punteras del sector TIC.
             
-                Si eres <b>candidato</b> en búsqueda activa de trabajo, no dudes en registrar tu perfil en nuestra plataforma, así las empresas podrán valorar tu candidatura para las posiciones vacantes.
+                Si eres candidato en búsqueda activa de trabajo, no dudes en registrar tu perfil en nuestra plataforma, así las empresas podrán valorar tu candidatura para las posiciones vacantes.
                 
-                Si eres <b>empresa</b>, registrate con nostros para poder encontra el mejor candidato para tu proyecto.</p>            
+                Si eres empresa, registrate con nostros para poder encontra el mejor candidato para tu proyecto.</p>            
         </div>
     );
 };
