f75d85097da47a31ea7fe883fbcb1d4bbe80abb2
Moi <mmena4@hotmail.com>
2021-01-24 15:32:22 +0100

    Visualización de perfil y tarjetas usuarios finalizadas

--- resources/js/components/userCard/UserCard.js
+++ resources/js/components/userCard/UserCard.js
@@ -8,10 +8,10 @@ function UserCard() {
             <div className="userCard-Container__top">
                 <img src="../assets/lorena.png"/>
                 <div className="userCard-Container__data">
-                    <span>Lorena Criado</span>
-                    <span>lorena@gmail.com</span>
-                    <span>Front-End</span>
-                    <span>HTML, CSS, JavaScript, React</span>
+                    <h3>Lorena Criado</h3>
+                    <h3>lorena@gmail.com</h3>
+                    <h3>Front-End</h3>
+                    <h3>HTML, CSS, JavaScript, React</h3>
                 </div>
                 
                 
@@ -20,16 +20,14 @@ function UserCard() {
             <div className="userCard-Container__bottom">
                 <div className="userCard-Container__text1">
                     <h3>Experiencia laboral</h3>
-                    <p>5</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et sit donec ipsum suspendisse leo at. Volutpat adipiscing non quisque ullamcorper arcu at eget rhoncus dolor. At ut non rhoncus et commodo pellentesque. Nec quam at rhoncus sollicitudin sollicitudin cursus tellus. Pulvinar ipsum amet, purus ligula justo, sit. Volutpat in imperdiet scelerisque ornare dictumst. Fermentum elementum risus leo vestibulum enim.</p>
                 </div>
                 <div className="userCard-Container__text2">
                     <h3>Experiencia académica</h3>
-                    <p>Front-End developer, CSS, Ract y JavaScript</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et sit donec ipsum suspendisse leo at. Volutpat adipiscing non quisque ullamcorper arcu at eget rhoncus dolor. At ut non rhoncus et commodo pellentesque. Nec quam at rhoncus sollicitudin sollicitudin cursus tellus. Pulvinar ipsum amet, purus ligula justo, sit. Volutpat in imperdiet scelerisque ornare dictumst.Fermentum elementum risus leo vestibulum enim. </p>
+
                 </div>                
             </div>  
-
-
-
         </div>
     );
 };
