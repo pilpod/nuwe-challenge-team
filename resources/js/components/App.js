380aa8f3eca26a7e04582523dd340dd2d0cb76da
vanessa <vanessapcbarros@gmail.com>
2021-01-24 15:51:49 +0100

    feat: add userprofile component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Hero from "./hero/Hero";
 import Footer from "./footer/Footer";
+import UserProfile from "./user-profile/UserProfile";
 
 function App() {
     return (
         <div className="container">
             <Hero />
+            <UserProfile />
             <Footer />
         </div>
     );
