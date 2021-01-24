942008b3e088d232cc9dc4bed10b965a8f1654aa
pilpod <67429595+pilpod@users.noreply.github.com>
2021-01-24 12:09:34 +0100

    pasted testing database user and workexperiences

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,18 +5,21 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_I_can_add_user_in_db()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
 
-        $response->assertStatus(200);
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('users', ['name' => $user->name]);
     }
 }
